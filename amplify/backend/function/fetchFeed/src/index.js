bd84c2576c42cce0edbe4967511fea4e7629cb54
Phil Gresham <philgresham@gmail.com>
2021-01-20 00:06:47 -0800

    FetchFeed: Add extra key to DDB params

--- amplify/backend/function/fetchFeed/src/index.js
+++ amplify/backend/function/fetchFeed/src/index.js
@@ -1,45 +1,61 @@
 /* Amplify Params - DO NOT EDIT
+	API_TSUNAMIALERT_ALERTTABLE_ARN
+	API_TSUNAMIALERT_ALERTTABLE_NAME
+	API_TSUNAMIALERT_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */ /* Amplify Params - DO NOT EDIT
 	ENV
 	REGION
 
-  Amplify Params - DO NOT EDIT */const Parser = require('rss-parser');
+  Amplify Params - DO NOT EDIT */ const Parser = require('rss-parser');
 const AWS = require('aws-sdk');
 const { promisify } = require('es6-promisify');
 
 AWS.config.update({ region: process.env.REGION });
 
-const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const dynamodb = new AWS.DynamoDB({
+  apiVersion: '2012-08-10',
+  region: process.env.REGION,
+});
 const parser = new Parser();
 
-const baseParams = {
-  TableName: process.env.API_TSUNAMIALERT_ALERTTABLE_NAME,
-};
+const TableName = process.env.API_TSUNAMIALERT_ALERTTABLE_NAME;
 
 exports.handler = async ({ url }) => {
   console.log({ url });
   let feed = await parser.parseURL(url);
 
-  const getItemPromise = promisify(dynamodb.getItem);
+  const getItemPromise = promisify(dynamodb.getItem.bind(dynamodb));
 
   const itemsPromises = [];
 
   feed.items.forEach((item) => {
-    const { id } = item;
+    const { id, pubDate } = item;
     const uuidIdx = id.search(/uuid\:/);
     if (uuidIdx === -1) return; // id is not formatted as expected
     const uuid = id.slice(uuidIdx + 5);
     const params = {
-      ...baseParams,
+      TableName,
       Key: {
         id: {
           S: uuid,
+        },
+        pubDate: {
+          S: pubDate,
         },
       },
     };
     itemsPromises.push(
       getItemPromise(params)
-        .then((response) => console.log({ response }))
-        .catch((err) => console.error({ err })),
+        .then((response) => {
+          console.log({ response });
+          return response;
+        })
+        .catch((err) => {
+          console.error({ err });
+          return err;
+        }),
     );
   });
   const results = await Promise.all(itemsPromises);
